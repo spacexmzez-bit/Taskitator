a3718dbdcacb8213cd777cf22aa1b3ca3cce4a63
spacexmzez-bit <spacexmzez@gmail.com>
2026-09-15 23:32:28 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,55 @@
+// sw.js
+const CACHE_NAME = 'taskitator-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './general.html',
+    './stats.html',
+    './settings.html',
+    './trash.html',
+    './style.css?v=14',
+    './manifest.json'
+];
+
+// Install: pre-cache static application shell
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+// Activate: purge outdated cache versions
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Fetch: serve from cache first, fall back to network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request).catch(() => {
+                // If offline and request is an HTML navigation, serve index.html
+                if (event.request.headers.get('accept')?.includes('text/html')) {
+                    return caches.match('./index.html');
+                }
+            });
+        })
+    );
+});
