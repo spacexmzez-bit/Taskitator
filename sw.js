573897d1f294b7299068747aa38eb1d2c473d863
spacexmzez-bit <spacexmzez@gmail.com>
2026-09-19 17:23:11 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js
 /**
  * Taskitator Service Worker
- * Version: taskitator-v25
+ * Version: taskitator-v26
  * 
  * Features:
  * - Network-First for core shell assets with offline cache fallback.
